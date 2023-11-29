8ab52e46d7cb4a21e43390af0d38e44c0bc541bb
A Sandhu <a3sandhu@ucsd.edu>
2023-11-29 11:27:04 -0800

    test5

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,82 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const textFields = document.querySelectorAll('input[type="text"], input[type="email"], textarea');
+
+    textFields.forEach(function (field) {
+        field.addEventListener('input', function (event) {
+            const inputValue = event.target.value;
+            const pattern = /^[a-zA-Z0-9@.]+$/; // Modify the pattern as needed
+
+            if (!pattern.test(inputValue)) {
+                flashField(field);
+                displayErrorMessage(field, 'Illegal character detected');
+                preventIllegalInput(event);
+            } else {
+                hideErrorMessage(field);
+            }
+        });
+    });
+
+    const textArea = document.getElementById('comments');
+    const charCountElement = document.getElementById('char-count');
+    const commentsInfo = document.getElementById('info_m');
+
+    textArea.addEventListener('input', function () {
+        updateCharCount();
+    });
+
+    function updateCharCount() {
+        const maxChars = textArea.maxLength;
+        const remainingChars = maxChars - textArea.value.length;
+
+        charCountElement.textContent = remainingChars;
+
+        // Update style based on remaining characters
+        if (remainingChars <= 20 && remainingChars > 10) {
+            commentsInfo.classList.remove('error');
+            commentsInfo.classList.add('warn');
+        } else if (remainingChars <= 10) {
+            commentsInfo.classList.remove('warn');
+            commentsInfo.classList.add('error');
+        } else {
+            commentsInfo.classList.remove('warn', 'error');
+        }
+    }
+
+    function flashField(field) {
+        field.style.transition = 'background-color 0.3s ease';
+        field.style.backgroundColor = '#FFD6D6'; // Change to your preferred flash color
+        setTimeout(function () {
+            field.style.backgroundColor = '';
+        }, 300);
+    }
+
+    function displayErrorMessage(field, message) {
+        const errorMessage = document.getElementById('error_m');
+        errorMessage.textContent = message;
+        errorMessage.style.display = 'block';
+        setTimeout(function () {
+            errorMessage.style.opacity = '0';
+            setTimeout(function () {
+                errorMessage.style.display = 'none';
+                errorMessage.style.opacity = '1';
+            }, 500);
+        }, 2000);
+    }
+
+    function hideErrorMessage(field) {
+        const errorMessage = document.getElementById('error_m');
+        errorMessage.style.display = 'none';
+        errorMessage.textContent = '';
+    }
+
+    function preventIllegalInput(event) {
+        event.preventDefault();
+        return false;
+    }
+});
+
+
+
 // Function to toggle dark mode
 function toggleDarkMode() {
     const body = document.body;
