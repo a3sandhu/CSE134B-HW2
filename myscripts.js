7d281c3a9d8571d8f8d9dc47e2502175393cc693
A Sandhu <a3sandhu@ucsd.edu>
2023-11-29 11:49:12 -0800

    test9

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,14 @@
+const email = document.getElementById("mail");
+const form = document.querySelector("form");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an email address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
 const myElement = document.getElementById('comments');
 
 // Add an event listener for the "keypress" event
@@ -10,8 +21,30 @@ myElement.addEventListener("keypress", (event) => {
     }
 });
 
-
-
+form.addEventListener("submit", (event) => {
+    // if the email field is valid, we let the form submit
+    if (!form.validity.valid) {
+      // If it isn't, we display an appropriate error message
+      showError();
+      // Then we prevent the form from being sent by canceling the event
+      event.preventDefault();
+    }
+  });
+  function showError() {
+    if (myElement.validity.valueMissing) {
+      // If the field is empty,
+      // display the following error message.
+      emailError.textContent = "You need to enter an email address.";
+    } else if (myElement.validity.typeMismatch) {
+      // If the field doesn't contain an email address,
+      // display the following error message.
+      emailError.textContent = "Entered value needs to be an email address.";
+    } else if (myElement.validity.tooShort) {
+      // If the data is too short,
+      // display the following error message.
+      error_m.textContent = `Invalid Comment!`;
+    }
+}
 // Function to toggle dark mode
 function toggleDarkMode() {
     const body = document.body;
