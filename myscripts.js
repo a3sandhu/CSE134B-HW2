16ed5635b507f9685e6cfacddead2ea007d5ebc5
A Sandhu <a3sandhu@ucsd.edu>
2023-11-29 07:10:33 -0800

    attempt to save dark mode

--- myscripts.js
+++ myscripts.js
@@ -1,15 +1,26 @@
+// Function to toggle dark mode
 function toggleDarkMode() {
     const body = document.body;
     body.classList.toggle('dark-mode');
+    
+    // Save theme preference to local storage
+    const isDarkMode = body.classList.contains('dark-mode');
+    localStorage.setItem('darkMode', isDarkMode);
 }
 
 // Function to handle the button click event
 function myFunction() {
     toggleDarkMode();
 }
-/*<script>
-    function myFunction() {
-        var element = document.body;
-        element.classList.toggle("dark-mode");
+
+// Function to check and apply the theme preference on page load
+function applySavedTheme() {
+    const savedTheme = localStorage.getItem('darkMode');
+
+    if (savedTheme === 'true') {
+        document.body.classList.add('dark-mode');
     }
-</script>*/
+}
+
+// Apply saved theme on page load
+applySavedTheme();
