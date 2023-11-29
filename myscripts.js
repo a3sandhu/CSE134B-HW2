1307c76bf62af997891e895bbb2f594aee54b3c4
A Sandhu <a3sandhu@ucsd.edu>
2023-11-29 07:06:14 -0800

    dark mode add js file

---
+++ myscripts.js
@@ -0,0 +1,15 @@
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+}
+
+// Function to handle the button click event
+function myFunction() {
+    toggleDarkMode();
+}
+/*<script>
+    function myFunction() {
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+    }
+</script>*/
